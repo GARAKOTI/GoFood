e6c3aaa8f4d277d9395ca13a2efd21233898da56
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2023-11-15 21:28:03 +0530

    "Adding "

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ app.get("/", (req, res) => {
   res.send(`listening on ${port}`);
 });
 
+app.use('/api', require('./Routes/CreateUser'));
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
